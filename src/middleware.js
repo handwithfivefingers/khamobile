d80910e614fd97d50711b74d41e9d080949fdedd
handwithfivefingers <handgod1995@gmail.com>
2022-12-12 11:22:57 +0700

    fix path mioddleware

--- src/middleware.js
+++ src/middleware.js
@@ -2,27 +2,27 @@ import { NextResponse } from 'next/server'
 import axios from 'configs/axiosInstance'
 
 export const config = {
-  matcher: ['/admin/:path*', '/admin'],
+  matcher: '/admin/:path*',
 }
 export async function middleware(request) {
-  if (request.nextUrl.pathname.includes('/admin')) {
-    // const resp = await axios.post('/authenticate')
+  // if (request.nextUrl.pathname.includes('/admin')) {
+  // const resp = await axios.post('/authenticate')
 
-    const requestHeaders = new Headers(request.headers)
+  const requestHeaders = new Headers(request.headers)
 
-    const path = `${process.env.API}/api/authenticate`
+  const path = `${process.env.API}/api/authenticate`
 
-    const resp = await fetch(path, {
-      method: 'POST',
-      credentials: 'include',
-      withCredentials: true,
-      headers: requestHeaders,
-    })
+  const resp = await fetch(path, {
+    method: 'POST',
+    credentials: 'include',
+    withCredentials: true,
+    headers: requestHeaders,
+  })
 
-    // const resp = await axios.post('/api/authenticate')
-    console.log('middleware ---> ', resp)
-    if (resp.status === 200) {
-      return NextResponse.next()
-    } else return NextResponse.redirect(new URL('/login', request.url))
-  }
+  // const resp = await axios.post('/api/authenticate')
+  console.log('middleware ---> ', resp)
+  if (resp.status === 200) {
+    return NextResponse.next()
+  } else return NextResponse.redirect(new URL('/login', request.url))
+  // }
 }
