b6b4a4ffae678a6f7ab92f3e78c811682b02ecac
handwithfivefingers <handgod1995@gmail.com>
2023-01-12 07:25:41 +0700

    update build time

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 // const webpack = require('webpack');
 // import webpack from 'webpack';
 // const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-// import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin'
 
 const nextConfig = {
   /* config options here */
-   images: {
+  images: {
     formats: ['image/webp'],
     domains: [
       'via.placeholder.com', //facebook
@@ -37,6 +37,53 @@ const nextConfig = {
     APP_NAME: process.env.APP_NAME,
     GG_CLIENT_ID: process.env.GG_CLIENT_ID,
     GG_CLIENT_SECRET: process.env.GG_CLIENT_SECRET,
+  },
+  webpack: (config) => {
+    // uglifyjs-webpack-plugin
+    config.plugins = config.plugins.filter((p) => p.constructor.name !== 'UglifyJsPlugin')
+    config.plugins.push(
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          exclude: /react\.js/,
+          parallel: true,
+          cache: true,
+          sourceMap: false,
+          compress: {
+            arrows: false,
+            booleans: false,
+            collapse_vars: false,
+            comparisons: false,
+            hoist_funs: false,
+            hoist_props: false,
+            hoist_vars: false,
+            if_return: false,
+            inline: false,
+            join_vars: false,
+            keep_infinity: true,
+            loops: false,
+            negate_iife: false,
+            properties: false,
+            reduce_funcs: false,
+            reduce_vars: false,
+            sequences: false,
+            side_effects: false,
+            switches: false,
+            top_retain: false,
+            toplevel: false,
+            typeofs: false,
+            unused: false,
+
+            // Switch off all types of compression except those needed to convince
+            // react-devtools that we're using a production build
+            conditionals: true,
+            dead_code: true,
+            evaluate: true,
+          },
+          mangle: true,
+        },
+      }),
+    )
+    return config
   },
 }
 
